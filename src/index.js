f78bf9aa50cf665de128c696d9ac7187e47110d7
sidneva.e.d <sidneva.e.d@gmail.com>
2023-12-08 12:33:09 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,125 @@
+import './styles/main.css'
+import logo from './assets/logo.svg'
+import user from './assets/user.svg'
+import menu from './assets/menu.svg'
+import aboutCourse from './assets/aboutCourse.png'
+import courseCover from './assets/courseCover.png'
+import courseCover1 from './assets/courseCover.png'
+import courseCover2 from './assets/courseCover.png'
+import courseCover3 from './assets/courseCover.png'
+import courseCover4 from './assets/courseCover.png'
+import timeCircle from './assets/TimeCircle.svg'
+import timeCircle1 from './assets/TimeCircle.svg'
+import timeCircle2 from './assets/TimeCircle.svg'
+import timeCircle3 from './assets/TimeCircle.svg'
+import timeCircle4 from './assets/TimeCircle.svg'
+import star from './assets/Star.svg'
+import star1 from './assets/Star.svg'
+import star2 from './assets/Star.svg'
+import star3 from './assets/Star.svg'
+import star4 from './assets/Star.svg'
+import youtube from './assets/social/youtube.svg'
+import youtube1 from './assets/social/youtube.svg'
+import vk from './assets/social/vk.svg'
+import vk1 from './assets/social/vk.svg'
+import frame_1 from './assets/social/Frame_1.svg'
+import frame_2 from './assets/social/Frame_1.svg'
+import calendar from './assets/Calendar.svg'
+import calendar1 from './assets/Calendar.svg'
+import calendar2 from './assets/Calendar.svg'
+import eye from './assets/Eye.svg'
+import eye1 from './assets/Eye.svg'
+import eye2 from './assets/Eye.svg'
+
+const logoImg = document.getElementById('logoImg')
+logoImg.src = logo
+
+const userImg = document.getElementById('userImg')
+userImg.src = user
+
+const menuImg = document.getElementById('menuImg')
+menuImg.src = menu
+
+const aboutCourseImg = document.getElementById('aboutCourseImg')
+aboutCourseImg.src = aboutCourse
+
+const courseCoverImg = document.getElementById('courseCoverImg')
+courseCoverImg.src = courseCover
+
+const courseCoverImg1 = document.getElementById('courseCoverImg1')
+courseCoverImg1.src = courseCover1
+
+const courseCoverImg2 = document.getElementById('courseCoverImg2')
+courseCoverImg2.src = courseCover2
+
+const courseCoverImg3 = document.getElementById('courseCoverImg3')
+courseCoverImg3.src = courseCover3
+
+const courseCoverImg4 = document.getElementById('courseCoverImg4')
+courseCoverImg4.src = courseCover4
+
+const timeCircleImg = document.getElementById('timeCircleImg')
+timeCircleImg.src = timeCircle
+
+const timeCircleImg1 = document.getElementById('timeCircleImg1')
+timeCircleImg1.src = timeCircle1
+
+const timeCircleImg2 = document.getElementById('timeCircleImg2')
+timeCircleImg2.src = timeCircle2
+
+const timeCircleImg3 = document.getElementById('timeCircleImg3')
+timeCircleImg3.src = timeCircle3
+
+const timeCircleImg4 = document.getElementById('timeCircleImg4')
+timeCircleImg4.src = timeCircle4
+
+const starImg = document.getElementById('starImg')
+starImg.src = star
+
+const starImg1 = document.getElementById('starImg1')
+starImg1.src = star1
+
+const starImg2 = document.getElementById('starImg2')
+starImg2.src = star2
+
+const starImg3 = document.getElementById('starImg3')
+starImg3.src = star3
+
+const starImg4 = document.getElementById('starImg4')
+starImg4.src = star4
+
+const youtubeImg = document.getElementById('youtubeImg')
+youtubeImg.src = youtube
+
+const youtubeImg1 = document.getElementById('youtubeImg1')
+youtubeImg1.src = youtube1
+
+const vkImg = document.getElementById('vkImg')
+vkImg.src = vk
+
+const vkImg1 = document.getElementById('vkImg1')
+vkImg1.src = vk1
+
+const frameImg = document.getElementById('frameImg')
+frameImg.src = frame_1
+
+const frameImg1 = document.getElementById('frameImg1')
+frameImg1.src = frame_2
+
+const eyeImg = document.getElementById('eyeImg')
+eyeImg.src = eye
+
+const eyeImg1 = document.getElementById('eyeImg1')
+eyeImg1.src = eye1
+
+const eyeImg2 = document.getElementById('eyeImg2')
+eyeImg2.src = eye2
+
+const calendarImg = document.getElementById('calendarImg')
+calendarImg.src = calendar
+
+const calendarImg1 = document.getElementById('calendarImg1')
+calendarImg1.src = calendar1
+
+const calendarImg2 = document.getElementById('calendarImg2')
+calendarImg2.src = calendar2
